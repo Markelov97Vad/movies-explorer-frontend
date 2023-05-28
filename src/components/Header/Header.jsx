6059ec85845b90a16f13625ac0bf97bbdc7e0af9
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-05-28 11:08:13 +0300

    feat: headder

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from '../../images/logo.svg'
+import './Header.css'
+
+function Header () {
+  return (
+    <header className="header root__header">
+      <img src={logo} alt="" className="header__logo"/>
+      <nav className="header__menu">
+        <span className="header__link">Регистрация</span>
+        <button className="header__button">Войти</button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
