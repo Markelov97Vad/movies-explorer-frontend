8015ed9d2667abba58f24142ee1c76688f690351
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-05-28 21:33:12 +0300

    feat: AboutProject

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
-import './Main.css'
+import AboutProject from './AboutProject/AboutProject';
 import Promo from './Promo/Promo';
+import './Main.css'
 
 function Main () {
   return (
     <main className='content'>
       <Promo />
+      <AboutProject />
     </main>
   )
 }
