4d7f4f56f3fd334a8f6ebdea26682ca7051c1980
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-05-28 15:46:29 +0300

    feat: promo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
-
+import Main from '../Main/Main';
 
 function App() {
   return ( 
     <>
       <Header />
+      <Main />
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import logo from '../../images/logo.svg'
 import './Header.css'
 
 function Header () {
   return (
     <header className="header root__header">
-      <img src={logo} alt="" className="header__logo"/>
+      <img src={logo} alt="логотип Movie explorer" className="header__logo"/>
       <nav className="header__menu">
         <span className="header__link">Регистрация</span>
         <button className="header__button">Войти</button>
