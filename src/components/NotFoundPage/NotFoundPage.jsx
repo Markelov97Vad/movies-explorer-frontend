312ee58c7d9e75aa4a316ea910b998615e660d7b
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-06-13 10:07:40 +0300

    fix: NotFoundPage

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -4,9 +4,13 @@ import './NotFoundPage.css'
 function NotFoundPage() {
   return ( 
     <div className='not-found-page'>
-      <h2 className='not-found-page__title'>404</h2>
-      <span className='not-found-page__subtitle'>Страница не найдена</span>
-      <NavLinkSign to='/' text='Назад' place='not-found' type='link' color='blue'/>
+      <div className='not-found-page__container'>
+        <div className='not-found-page__wrapper'>
+          <h2 className='not-found-page__title'>404</h2>
+          <span className='not-found-page__subtitle'>Страница не найдена</span>
+        </div>
+        <NavLinkSign to='/' text='Назад' place='not-found' type='link' color='blue'/>
+      </div>
     </div>
    );
 }
