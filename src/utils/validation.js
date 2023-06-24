136475fa8bbf3b881aa0365a2650be0af39be838
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-06-24 22:11:16 +0300

    refact: pattern regex

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,5 +1,6 @@
-const regexName = "[A-я-\\s]{2,30}";
-const regexEmail = "^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$";
+// const regexName = '[A-я-\s]{2,30}';
+const regexName = "^[A-я]{2,30}";
+const regexEmail = "\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$/";
 const regexPassword = "(?=.*[A-z])(?=.*\\d)(?=.)(?=.{1,}).*";
 
 export {
